1b78dac8ceeee8a5796d83df8286afd3416fa53b
praveen <praveenpenv@gmail.com>
2020-02-27 15:51:24 +0530

    implemented crud operations

--- src/main/java/application/UserController.java
+++ src/main/java/application/UserController.java
@@ -1,19 +1,46 @@
 package application;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class UserController {
 
 	@Autowired
 	private UserService userService;
+
+	@RequestMapping(method = RequestMethod.POST, value = "/users")
+	public User createUser(@RequestBody User user) {
+		return userService.create(user);
+	}
+	
+	@RequestMapping("/users")
+	public List<User> getAllUsers() {
+		return userService.getAll();
+	}
 	
 	@RequestMapping("/users/{userId}")
 	public User readUser(@PathVariable String userId) {
 		 
 		return userService.read(userId);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT, value = "/users/{userId}")
+	public User updateUser(@Valid @RequestBody User user, @PathVariable String userId) {
+		return userService.update(userId, user);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/users/{userId}")
+	public void deleteUser(@PathVariable String userId) {
+		userService.delete(userId);
 	}
 }
--- src/main/java/application/UserService.java
+++ src/main/java/application/UserService.java
@@ -1,9 +1,13 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 
 @Service
 public class UserService {
@@ -17,6 +21,30 @@ public class UserService {
 	
 	public User read(String userId) {
 		return dynamoDBMapper.load(User.class, userId);
+	}
+
+	public User create(User user) {
+		
+		dynamoDBMapper.save(user);
+		return user;
+	}
+
+	public List<User> getAll() {
+		List<User> users = new ArrayList<>();
+		users=dynamoDBMapper.scan(User.class,new DynamoDBScanExpression());
+		return users;
+	}
+
+	public User update(String userId, User user) {
+		dynamoDBMapper.load(User.class, userId);
+		dynamoDBMapper.save(user);
+		return user;
+	}
+
+	public void delete(String userId) {
+		User user = dynamoDBMapper.load(User.class, userId);
+		dynamoDBMapper.delete(user);
+		
 	}
 
 }
